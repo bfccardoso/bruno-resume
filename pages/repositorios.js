4a8bd9639833dfabb6b638b6245c4f42e1343f65
bfccardoso <bfccardoso@gmail.com>
2022-01-10 21:27:04 -0300

    Correção ortográfica

--- pages/repositorios.js
+++ pages/repositorios.js
@@ -4,7 +4,7 @@ const Index = ({repos, user}) => {
     return(
         <div className="container mx-auto">
             <h1 className="text-5xl">Meus repositórios</h1>
-            <h2 className="font-bold text-3xl">Mues repositórios no GitHub</h2>
+            <h2 className="font-bold text-3xl">Meus repositórios no GitHub</h2>
             <p>Github stats: public repos: {user.public_repos} / public_gists: {user.public_gists} / followers: {user.followers}</p>
             {repos.map(repo => {
                 return(
